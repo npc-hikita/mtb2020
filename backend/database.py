b86795429304a0558f32f9e1885315b9de9cf131
hikita <hikita@nihon-protec.co.jp>
2021-01-12 20:55:03 +0000

    Heroku DB対応

--- backend/database.py
+++ backend/database.py
@@ -5,7 +5,6 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.environ.get(
     'DATABASE_URL') or "postgresql://postgres:passmtb2020@db/postgres"
-print("SQLALCHEMY_DATABASE_URL is " + SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
