7a8a1a07d11590373fe73a626aca2e7f8e7dd076
hikita <hikita@nihon-protec.co.jp>
2021-01-12 20:26:27 +0000

    update

--- backend/database.py
+++ backend/database.py
@@ -5,6 +5,7 @@ import os
 
 SQLALCHEMY_DATABASE_URL = os.environ.get(
     'DATABASE_URL') or "postgresql://postgres:passmtb2020@db/postgres"
+print("SQLALCHEMY_DATABASE_URL is " + SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
